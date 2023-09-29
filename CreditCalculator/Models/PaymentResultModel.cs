bde5b5c36cddbf8d26a368ac63bdc38c8d7ccad5
MinaevaAM <MinaevaAM@DESKTOP-CBD2ET9>
2023-09-29 16:45:56 +0500

    Добавьте файлы проекта.

---
+++ CreditCalculator/Models/PaymentResultModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Kalkulator.Models
+{
+    public class PaymentResultModel
+    {
+        public int ID { get; set; }
+        public string? Date { get; set; }
+        public decimal Body { get; set; }
+        public decimal Percent { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
