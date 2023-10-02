3c90aa826531e756b7e3a8e7f3f27a3fb854c0eb
Albert7521 <Minaev.a@kassaone.ru>
2023-10-02 15:03:41 +0500

    Add DI

--- CreditCalculator/Controllers/HomeController.cs
+++ CreditCalculator/Controllers/HomeController.cs
@@ -1,16 +1,59 @@
 ﻿using Kalkulator.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using Microsoft.Extensions.Hosting;
 
 namespace Kalkulator.Controllers
 {
+    public interface ICreditCalculationService
+    {
+        List<PaymentResultModel> CalculatePayments(CreditInformationModel creditInformation);
+    }
+
+    public class CreditCalculationService : ICreditCalculationService
+    {
+        public List<PaymentResultModel> CalculatePayments(CreditInformationModel creditInformation)
+        {
+            decimal balance = creditInformation.Sum;
+            List<PaymentResultModel> results = new List<PaymentResultModel>();
+            for (int j = 0; j < creditInformation.Time; j++)
+            {
+                PaymentResultModel result = CreateResultModel(creditInformation, j, ref balance);
+                results.Add(result);
+            }
+            return results;
+        }
+
+        private PaymentResultModel CreateResultModel(CreditInformationModel creditInformation, int index, ref decimal balance)
+        {
+            PaymentResultModel result = new PaymentResultModel();
+            result.Date = DateTime.Now.AddDays(index + 1).ToString("d MMMM yyyy") + " г.";
+            result.ID = index + 1;
+            decimal i = creditInformation.Rate / 12 / 100;
+            decimal power = (decimal)Math.Pow((double)(1 + i), creditInformation.Time);
+            decimal сoef = (i * power) / (power - 1);
+            decimal total = сoef * creditInformation.Sum;
+            decimal percent = balance / 12 / 100 * creditInformation.Rate;
+            decimal part = total - percent;
+            balance -= part;
+            result.Balance = balance;
+            result.Percent = percent;
+            result.Body = part;
+            return result;
+        }
+    }
+
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICreditCalculationService _creditCalculationService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICreditCalculationService creditCalculationService)
         {
             _logger = logger;
+            _creditCalculationService = creditCalculationService;
         }
 
         public IActionResult Index()
@@ -29,37 +72,12 @@ namespace Kalkulator.Controllers
         {
             if (ModelState.IsValid)
             {
-                decimal balance = creditInformation.Sum;
-                decimal totalOverpaid = 0;
-                List<PaymentResultModel> results = new List<PaymentResultModel>();
-                for (int j = 0; j < creditInformation.Time; j++)
-                {
-                    PaymentResultModel result = CreateResultModel(creditInformation, j, ref balance);
-                    totalOverpaid += result.Percent;
-                    results.Add(result);
-                }
+                var results = _creditCalculationService.CalculatePayments(creditInformation);
+                decimal totalOverpaid = results.Sum(x => x.Percent);
                 TempData["totalOverpaid"] = totalOverpaid;
                 return View("Results", results);
             }
             return View(creditInformation);
-        }
-
-        private PaymentResultModel CreateResultModel(CreditInformationModel creditInformation, int index, ref decimal balance)
-        {
-            PaymentResultModel result = new PaymentResultModel();
-            result.Date = DateTime.Now.AddDays(index + 1).ToString("d MMMM yyyy") + " г.";
-            result.ID = index + 1;
-            decimal i = creditInformation.Rate / 12 / 100; 
-            decimal power = (decimal)Math.Pow((double)(1 + i), creditInformation.Time);
-            decimal сoef = (i * power) / (power - 1); 
-            decimal total = сoef * creditInformation.Sum;
-            decimal percent = balance / 12 / 100 * creditInformation.Rate;
-            decimal part = total - percent;
-            balance -= part;
-            result.Balance = balance;
-            result.Percent = percent;
-            result.Body = part;
-            return result;
         }
     }
 }
--- CreditCalculator/Program.cs
+++ CreditCalculator/Program.cs
@@ -1,36 +1,17 @@
+using Kalkulator.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace Kalkulator
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+            Startup start = new Startup();
+            start.Configure (args);
+            
         }
     }
 }
