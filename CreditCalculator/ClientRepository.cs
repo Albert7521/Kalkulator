646571c5e251310e0a858f94548ae3b7d2d7e842
Albert7521 <Minaev.a@kassaone.ru>
2023-10-05 09:40:59 +0500

    Splitting into separate files

---
+++ CreditCalculator/ClientRepository.cs
@@ -0,0 +1,43 @@
+﻿using Kalkulator.Models;
+using System.Data.Entity;
+
+namespace Kalkulator
+{
+    public class ClientRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(Client client)
+        {
+            _context.Clients.Add(client);
+            _context.SaveChanges();
+        }
+
+        public Client GetById(int id)
+        {
+            return _context.Clients.Find(id);
+        }
+
+        public void Update(Client client)
+        {
+            _context.Entry(client).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var client = GetById(id);
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+                _context.SaveChanges();
+            }
+        }
+
+    }
+}
--- CreditCalculator/Klient.cs
+++ CreditCalculator/Klient.cs
@@ -1,62 +1,15 @@
-﻿using System;
+﻿using Kalkulator.Models;
+using System;
 using System.Data;
 using System.Data.Entity;
 using System.Data.SqlClient;
-using static ClientManagement;
 
-public class ClientManagement
+namespace Kalkulator.Models
 {
-
-    public class Client
-    {
-
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-
-        // Другие свойства клиента
-
-    }
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Client> Clients { get; set; }
         // Другие DbSet'ы для других сущностей, если нужно
     }
-    public class ClientRepository
-    {
-        private readonly ApplicationDbContext _context;
-
-        public ClientRepository(ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public void Create(Client client)
-        {
-            _context.Clients.Add(client);
-            _context.SaveChanges();
-        }
-
-        public Client GetById(int id)
-        {
-            return _context.Clients.Find(id);
-        }
-
-        public void Update(Client client)
-        {
-            _context.Entry(client).State = EntityState.Modified;
-            _context.SaveChanges();
-        }
-
-        public void Delete(int id)
-        {
-            var client = GetById(id);
-            if (client != null)
-            {
-                _context.Clients.Remove(client);
-                _context.SaveChanges();
-            }
-        }
-
-    }
+    
 }
